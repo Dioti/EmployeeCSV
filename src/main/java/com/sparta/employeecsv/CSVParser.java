6e3f00bba5a6b9420c9a2784a481c541496e1154
Natasha <natasha.pearl08@gmail.com>
2021-10-07 10:03:10 +0100

    writes csv contents to a database

--- src/main/java/com/sparta/employeecsv/CSVParser.java
+++ src/main/java/com/sparta/employeecsv/CSVParser.java
@@ -22,9 +22,45 @@ public class CSVParser {
 
     private static Date strToDate(String str) {
         String[] values = str.split("/");
-        return Date.valueOf(String.format("%d-%02d-%02d", Integer.parseInt(values[2]),
+        return Date.valueOf(String.format("%d-%02d-%02d",
+                Integer.parseInt(values[2]),
                 Integer.parseInt(values[0]),
                 Integer.parseInt(values[1])));
+    }
+
+    private static int sanitiseID(int input) {
+        int output = input;
+        return output;
+    }
+
+    private static String sanitiseTitle(String input) {
+        String output = input;
+        return output;
+    }
+
+    private static String sanitiseName(String input) {
+        String output = input;
+        return output;
+    }
+
+    private static char sanitiseGender(char input) {
+        char output = input;
+        return output;
+    }
+
+    private static String sanitiseEmail(String input) {
+        String output = input;
+        return output;
+    }
+
+    private static Date sanitiseDate(Date input) {
+        Date output = input;
+        return output;
+    }
+
+    private static int sanitiseSalary(int input) {
+        int output = input;
+        return output;
     }
 
 }
